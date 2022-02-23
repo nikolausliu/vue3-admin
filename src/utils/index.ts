241e327c61f59bdea6ddb8c0aea2b26432cd0802
nikolausliu <18010855044@163.com>
2022-02-23 13:50:59 +0800

    feat: 休假申请列表

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,50 +1,4 @@
-// import { store } from '@/store'
-// import { useMenusStore } from '@/pinia/modules/menus'
-// import { MenuRaw } from '@/router/menus'
-import { AxiosResponse } from 'axios'
 import { RouteRecordRaw } from 'vue-router'
-
-// @TODO: children使用动态属性
-export interface NestedObj<T> {
-  [k: string]: any
-  children: Array<NestedObj<T>>
-}
-
-// @TODO: 添加children参数
-/**
- * 多维嵌套对象数组展平为一维对象数组
- * @param array
- */
-export function flatObjectArray<T>(array: Array<NestedObj<T>>): Array<NestedObj<T>> {
-  const res: Array<NestedObj<T>> = []
-  function recursive(target: Array<NestedObj<T>>) {
-    target.forEach((item: NestedObj<T>) => {
-      res.push(item)
-      // eslint-disable-next-line no-prototype-builtins
-      if (item.hasOwnProperty('children') && item.children.length) {
-        recursive(item.children)
-      }
-    })
-  }
-  recursive(array)
-  return res
-}
-
-/**
- * 把双引号转义成反斜杠加双引号，即 `"` 转义成 `\"`
- * @param str
- */
-export function escapeQuote(str: string): string {
-  return str.replace(/"/g, '\\"')
-}
-
-/**
- * 移除字符串中的反斜杠 `\`
- * @param str
- */
-export function removeBackslash(str: string): string {
-  return str.replace(/\\/g, '')
-}
 
 /**
  * 获取权限菜路由中的第一个路由path
@@ -69,113 +23,6 @@ export function getFirstRoutePath(): string {
   return '/system/menu'
 }
 
-/**
- * 下载文件
- * @param blob 要下载为文件的blob对象
- * @param filename 要下载的文件名称
- */
-export function downloadFile(blob: Blob, filename: string) {
-  const a = document.createElement('a')
-  const url = window.URL.createObjectURL(blob)
-  a.href = url
-  a.download = filename
-  a.click()
-  window.URL.revokeObjectURL(url)
-}
-
-/**
- * 封装根据axios response下载流文件的过程
- * @param response
- */
-export function downloadFileFromResponse(response: AxiosResponse) {
-  const blob = response.data.data
-  console.log('blob:', typeof blob, blob)
-  const headers = response.headers
-  // 研发环境和线上环境返回的头大小写不一样...
-  const disposition = headers['Content-disposition'] || headers['content-disposition'] || ''
-  // const disposition = headers["Content-disposition"] || "";
-  let filename = disposition.split('Filename=')[1] || disposition.split('filename=')[1]
-  filename = decodeURI(escape(filename))
-  console.log('disposition,filename', disposition, filename)
-  downloadFile(blob, filename)
-}
-
-export function downloadFileFromUrl(url: string, filename: string) {
-  fetch(url, {
-    headers: new Headers({
-      Origin: location.origin,
-    }),
-    mode: 'cors',
-  })
-    .then((res) => res.blob())
-    .then((blob) => {
-      downloadFile(blob, filename)
-    })
-}
-
-/**
- * blob对象转换为json
- * @param blob 要转换的
- */
-export function blob2Json(blob: Blob) {
-  return new Promise((resolve) => {
-    const reader = new FileReader()
-    reader.readAsText(blob)
-    reader.onload = (e: any) => {
-      console.log(e)
-      resolve(JSON.parse(e.target.result as string))
-    }
-  })
-}
-
-/**
- * 求数字数组中的中位数算法
- * @param arr
- */
-export function median(arr: number[]): number {
-  arr.sort()
-  if (arr.length % 2 === 0) {
-    return (arr[arr.length / 2 - 1] + arr[arr.length / 2]) / 2
-  } else {
-    return arr[Math.floor(arr.length / 2)]
-  }
-}
-
-/**
- * 深拷贝，直接简单用了
- */
-export function cloneDeep(obj: any) {
-  return JSON.parse(JSON.stringify(obj))
-}
-
-/**
- * 复制到剪贴板
- * @param value 要复制的字符串
- */
-export function copyToClipboard(value: string): Promise<boolean> {
-  const input = document.createElement('textarea')
-  input.style.position = 'absolute'
-  input.style.top = '9999px'
-  document.body.appendChild(input)
-  input.value = value
-  input.select()
-  document.execCommand('copy')
-  document.body.removeChild(input)
-  return Promise.resolve(true)
-}
-
-/**
- * Ant Modal组件，在非开发环境下关闭时，偶尔会出现其透明遮罩层 .ant-modal-wrap 没有被正确的隐藏
- * 这可能是一个bug
- * 这里先粗暴的通过操作dom解决一下
- */
-export function forceCloseModalWrap(selectorIndex = 0) {
-  const selectors = ['.ant-modal-wrap', '.ant-image-preview-wrap']
-  ;[].forEach.call(document.querySelectorAll(selectors[selectorIndex]), (item) => {
-    ;(item as HTMLElement).style.display = 'none'
-  })
-}
-
 export function generateRoute(menuList: Recordable[]) {
   const routes: RouteRecordRaw[] = []
   const deepList = (list: Recordable[]) => {
@@ -198,4 +45,26 @@ export function generateRoute(menuList: Recordable[]) {
   }
   deepList(menuList)
   return routes
+}
+
+export function formateDate(date: Date, rule: string): string {
+  let fmt = rule || 'yyyy-MM-dd hh:mm:ss'
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, date.getFullYear() + '')
+  }
+  const o = {
+    'M+': date.getMonth() + 1,
+    'd+': date.getDate(),
+    'h+': date.getHours(),
+    'm+': date.getMinutes(),
+    's+': date.getSeconds(),
+  }
+  const keys = Object.keys(o) as Array<keyof typeof o>
+  keys.forEach((k) => {
+    if (new RegExp(`(${k})`).test(fmt)) {
+      const val = o[k] + ''
+      fmt = fmt.replace(RegExp.$1, RegExp.$1.length === 1 ? val : ('00' + val).substr(val.length))
+    }
+  })
+  return fmt
 }
--- src/views/async/audit/Leave.vue
+++ src/views/async/audit/Leave.vue
@@ -1,10 +1,195 @@
 <template>
   <div>
+    <div class="query-form">
+      <a-form layout="inline" :colon="false">
+        <a-form-item label="审批状态">
+          <a-select
+            v-model:value="queryForm.applyState"
+            placeholder="请选择审批状态"
+            style="width: 200px"
+          >
+            <a-select-option value=""> 所有 </a-select-option>
+            <a-select-option v-for="item in applyStateArr" :key="item.value" :value="item.value">
+              {{ item.name }}
+            </a-select-option>
+          </a-select>
+        </a-form-item>
+        <a-form-item>
+          <a-button type="primary" @click="fetchTable">查询</a-button>
+        </a-form-item>
+      </a-form>
+    </div>
+
+    <div class="base-table">
+      <div class="action group-btn-wrap">
+        <a-button type="primary">申请休假</a-button>
+      </div>
+      <a-config-provider>
+        <template #renderEmpty>
+          <base-empty :is-error="table.error" @click="fetchTable" />
+        </template>
+        <a-table
+          :columns="columns"
+          :data-source="table.list"
+          :loading="table.loading"
+          :pagination="false"
+          :scroll="{ x: true }"
+          size="small"
+          row-key="_id"
+        >
+          <template #startTime="{ record }">
+            {{ formateDate(new Date(record.startTime), 'yyyy-MM-dd') }} ~
+            {{ formateDate(new Date(record.endTime), 'yyyy-MM-dd') }}
+          </template>
+          <template #applyType="{ record }">
+            {{ applyTypeArr.find((item) => item.value === record.applyType)?.name || '' }}
+          </template>
+          <template #applyState="{ record }">
+            {{ applyStateArr.find((item) => item.value === record.applyState)?.name || '' }}
+          </template>
+          <template #action="{ record }">
+            <span class="group-btn-wrap">
+              <a-button type="primary" size="small">查看</a-button>
+              <a-popconfirm title="确定要删除吗?" ok-text="是" cancel-text="否">
+                <a-button size="small" danger>作废</a-button>
+              </a-popconfirm>
+            </span>
+          </template>
+        </a-table>
+      </a-config-provider>
+      <div v-if="table.total" class="pagination-wrap">
+        <base-pagination
+          v-model:current="queryForm.pageNum"
+          v-model:pageSize="queryForm.pageSize"
+          :total="table.total"
+          @change="fetchTable"
+          @showSizeChange="fetchTable"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
+import { Form, message } from 'ant-design-vue'
+import { ColumnProps } from 'ant-design-vue/es/table/interface'
+import api from '@/api'
+import { formateDate } from '@/utils'
+
+const columns = [
+  {
+    key: 'orderNo',
+    dataIndex: 'orderNo',
+    align: 'center',
+    title: '单号',
+  },
+  {
+    key: 'startTime',
+    dataIndex: 'startTime',
+    align: 'center',
+    title: '休假时间',
+    slots: { customRender: 'startTime' },
+  },
+  {
+    key: 'leaveTime',
+    dataIndex: 'leaveTime',
+    align: 'center',
+    title: '休假时长',
+  },
+  {
+    key: 'applyType',
+    dataIndex: 'applyType',
+    align: 'center',
+    title: '休假类型',
+    slots: { customRender: 'applyType' },
+  },
+  {
+    key: 'reasons',
+    dataIndex: 'reasons',
+    align: 'center',
+    title: '休假原因',
+  },
+  {
+    key: 'createTime',
+    dataIndex: 'createTime',
+    align: 'center',
+    title: '申请时间',
+    slots: { customRender: 'createTime' },
+  },
+  {
+    key: 'auditUsers',
+    dataIndex: 'auditUsers',
+    align: 'center',
+    title: '审批人',
+  },
+  {
+    key: 'curAuditUserName',
+    dataIndex: 'curAuditUserName',
+    align: 'center',
+    title: '当前审批人',
+  },
+  {
+    key: 'applyState',
+    dataIndex: 'applyState',
+    align: 'center',
+    title: '审批状态',
+    slots: { customRender: 'applyState' },
+  },
+  {
+    key: 'action',
+    title: '操作',
+    align: 'center',
+    width: 150,
+    fixed: 'right',
+    slots: { customRender: 'action' },
+  },
+]
+
+const applyTypeArr = reactive([
+  { value: 1, name: '事假' },
+  { value: 2, name: '调休' },
+  { value: 3, name: '年假' },
+])
+const applyStateArr = reactive([
+  { value: 1, name: '待审批' },
+  { value: 2, name: '审批中' },
+  { value: 3, name: '审批拒绝' },
+  { value: 4, name: '审批通过' },
+  { value: 5, name: '作废' },
+])
+
+const queryForm = ref({
+  pageNum: 1,
+  pageSize: 10,
+  applyState: 1,
+})
+
+const table = reactive({
+  list: [] as Recordable[],
+  loading: true,
+  error: false,
+  total: 0,
+})
+const fetchTable = () => {
+  table.loading = true
+  api
+    .getApplyList(queryForm.value)
+    .then((res) => {
+      table.list = res.data.data.list
+      table.total = res.data.data.page.total
+      table.loading = false
+      table.error = false
+    })
+    .catch(() => {
+      table.list = []
+      tabel.total = 0
+      tabel.loading = false
+      table.error = true
+    })
+}
+
+fetchTable()
 </script>
 
 <script lang="ts">
